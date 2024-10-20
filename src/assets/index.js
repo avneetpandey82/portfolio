82653d1e9965f0d0c5dac08b03ace66599038f18
avneetpandey82 <avneetpandey82@gmail.com>
2024-10-20 00:20:26 -0400

    initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,97 @@
+import bwmap from "./backgrounds/bw-map.jpeg";
+import nairobi from "./backgrounds/nairobi.png";
+import whiteabstract from "./backgrounds/white-abstract.png";
+import worldmap from "./backgrounds/world-map.png";
+
+import logo from "./logo/logo-black.png";
+import logotext from "./logo/logo-text-black.svg";
+
+import avneet from "./personal/avneet.png";
+
+import backend from "./icons/backend.png";
+import frontend from "./icons/frontend.png";
+import cloud from "./icons/cloud.png";
+import ai from "./icons/ai_image.png";
+import github from "./icons/github.png";
+import close from "./icons/close.png";
+import download from "./icons/download.png";
+import downloadHover from "./icons/downloadHover.png";
+import pineapple from "./icons/pineapple.png";
+import pineappleHover from "./icons/pineappleHover.png";
+import menu from "./icons/menu.png";
+import resume from "./icons/resume.png";
+import send from "./icons/send.png";
+import sendHover from "./icons/sendHover.png";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import typescript from "./tech/typescript.png";
+import mySql from "./tech/mysql.png";
+import azure from "./tech/azure.png";
+import openai from "./tech/openai.png";
+import nextjs from "./tech/nextjs-icon.svg";
+import mongodb from "./tech/mongodb.png";
+import codingNinja from "./company/cnlogo.png";
+import vcLogo from "./company/vc_logo.jpeg";
+import freelancing from "./company/freelancing.jpeg";
+
+import lookseas from "./projects/lookseas.png";
+import walledgarden from "./projects/walledgarden.png";
+import patient_copilot from "./projects/patient-copilot.png";
+import inspection_center from "./projects/inspection-center.png";
+
+import loyalistCollege from "./education/loyalist_college.jpeg";
+import dce from "./education/dce.jpeg";
+
+export {
+  bwmap,
+  nairobi,
+  whiteabstract,
+  worldmap,
+  logo,
+  logotext,
+  avneet,
+  backend,
+  frontend,
+  ai,
+  cloud,
+  github,
+  close,
+  download,
+  downloadHover,
+  pineapple,
+  pineappleHover,
+  menu,
+  resume,
+  send,
+  sendHover,
+  css,
+  docker,
+  git,
+  html,
+  javascript,
+  nodejs,
+  openai,
+  nextjs,
+  azure,
+  mySql,
+  reactjs,
+  mongodb,
+  redux,
+  typescript,
+  codingNinja,
+  vcLogo,
+  freelancing,
+  lookseas,
+  walledgarden,
+  patient_copilot,
+  inspection_center,
+  loyalistCollege,
+  dce,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,279 @@
+import {
+  frontend,
+  backend,
+  cloud,
+  ai,
+  javascript,
+  typescript,
+  html,
+  nextjs,
+  css,
+  reactjs,
+  redux,
+  nodejs,
+  git,
+  docker,
+  mongodb,
+  mySql,
+  azure,
+  openai,
+  lookseas,
+  walledgarden,
+  inspection_center,
+  patient_copilot,
+  codingNinja,
+  vcLogo,
+  freelancing,
+  loyalistCollege,
+  dce,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  {
+    id: "work",
+    title: "Experience",
+  },
+];
+
+const services = [
+  {
+    title: "Full Stack Developer",
+    icon: frontend,
+  },
+  {
+    title: "Devops Engineer",
+    icon: backend,
+  },
+  {
+    title: "Cloud Administrator",
+    icon: cloud,
+  },
+  {
+    title: "Generative AI and LLM Creator",
+    icon: ai,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "Next JS",
+    icon: nextjs,
+  },
+
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "MySql",
+    icon: mySql,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "git",
+    icon: azure,
+  },
+
+  {
+    name: "docker",
+    icon: docker,
+  },
+  {
+    name: "open ai",
+    icon: openai,
+  },
+];
+
+const experiences = [
+  {
+    title: "Freelancer",
+    company_name: "Dedicated Client",
+    icon: freelancing,
+    iconBg: "#333333",
+    date: "May 2024 - Present",
+  },
+  {
+    title: "Associate Software Developer L-2",
+    company_name: "Valuecoders Software Services",
+    icon: vcLogo,
+    iconBg: "#333333",
+    date: "Oct 2022 - Mar 2024",
+  },
+  {
+    title: "Associate Software Developer L-1",
+    company_name: "Valuecoders Software Services",
+    icon: vcLogo,
+    iconBg: "#333333",
+    date: "Aug 2021 - Sep 2022",
+  },
+  {
+    title: "Junior Associate Software Developer",
+    company_name: "Valuecoders Software Services",
+    icon: vcLogo,
+    iconBg: "#333333",
+    date: "Jan 2021 - Aug 2021",
+  },
+  {
+    title: "Campus Ambassador",
+    company_name: "Conding Ninjas",
+    icon: codingNinja,
+    iconBg: "#333333",
+    date: "Aug 2020 - Dec 2020",
+  },
+];
+
+const projects = [
+  {
+    id: "lookseas",
+    name: "Lookseas",
+    description:
+      "It is a dynamic digital marketplace for survey and inspection documents.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: lookseas,
+    repo: "",
+    demo: "https://lookseas.com/",
+  },
+  {
+    id: "walledgardens",
+    name: "Walled Gardens",
+    description:
+      "Walled Gardens is a versatile platform empowering users to curate, monetize, and share their educational and artistic content in diverse formats.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: walledgarden,
+    repo: "",
+    demo: "https://walledgardens.ai/",
+  },
+  {
+    id: "patient_copilot",
+    name: "Patient Copilot",
+    description:
+      "This is my current project where we are focusing on the users medical needs by using Augmented and trained models of AI. Helping them understand their cancer lab reports, corresponding medicine suggestion and meta data about cancers to spread awareness about it",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "openai",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: patient_copilot,
+    repo: "",
+    demo: "https://www.patient-copilot.ai/",
+  },
+  {
+    id: "my_inspection_center",
+    name: "My Inspection Center",
+    description: `We provide you a tool to create a new House as well as new Boat Inspection for you. Register with us to look into all Inspections that are requested by our Users and can place your bids there.`,
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "reactjs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "nodejs",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: inspection_center,
+    repo: "https://github.com/shaqdeff/Movie-Metro",
+    demo: "https://my-inspection-center.vercel.app/",
+  },
+];
+
+const education = [
+  {
+    title: "Post Graduate",
+    company_name: "Loyalist College In Toronto",
+    icon: loyalistCollege,
+    iconBg: "#333333",
+    date: "May 2024 - Present",
+  },
+  {
+    title: "Bachelors in Technology",
+    company_name: "Dronacharya College of Engineering",
+    icon: dce,
+    iconBg: "#333333",
+    date: "May 2024 - Present",
+  },
+];
+
+export { services, technologies, experiences, projects, education };
